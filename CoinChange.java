fb478aab5c8e98b1d6a7aaac28699dd5e051dfdd
YogeshUpadhyay <yogeshh2021@gmail.com>
2020-07-29 23:20:20 +0530

    Coin change problem updated

--- CoinChange.java
+++ CoinChange.java
@@ -3,31 +3,33 @@ import java.util.*;
 
 class CoinChange {
     
-    
-    static long change(int[] coins,int s) {
-        long[][] ways = new long[coins.length+1][s+1];
+    static long change(List<Long> coins,int s) {
+        Arrays.fill(ways,-1);
         ways[0][0] = 1;
-        for(int i = 1;i <= coins.length;i++) {
-            for(int j = 0;j <= s;j++) {
-                if(coins[i-1] > j) {
+        for(int i = 0;i < n+1;i++) {
+            for(int j = 0;j <= s;s++) {
+                if(coins.get(i) > j) {
                     ways[i][j] = ways[i-1][j];
                 }else {
-                    ways[i][j] = ways[i-1][j]+ways[i][j-coins[i-1]];
+                    ways[i][j] = ways[i-1][j] + ways[i][j];
                 }
+
             }
         }
-        return ways[coins.length-1][s-1];
+        return ways[n][s];
     }
+    public static int  s,n;
+    public static long[][] ways = new long[n+1][s+1];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int s = sc.nextInt();
-        int n = sc.nextInt();
-        int[] coins = new int[n];
+        s = sc.nextInt();
+        n = sc.nextInt();
+        LinkedList<Long> coins = new LinkedList<>();
         for(int i = 0;i < n;i++) {
-            coins[i] = sc.nextInt();
+            long temp = sc.nextLong();
+            coins.add(temp);
         }
         sc.close();
-        int[][] arr = new int[coins.length+1][s+1];
 
         long result = change(coins,s);
         System.out.println(result);
